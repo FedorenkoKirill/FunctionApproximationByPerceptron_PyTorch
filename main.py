24e8d6c5a3147c32eaeb2adaf86931cb72eb166f
TK_User <tk_user@Mac-mini-TK-Admin.local>
2021-07-06 10:32:41 +0300

    changed neural network model

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
 import numpy as np
-import math
 import torch
 import torch.optim as optim
 import torch.nn as nn
-import torch.nn.functional as F
 from matplotlib import pyplot as plt
 
+#Задаем функцию, которую и будем аппроксимировать нейронной сетью
 def f(x):
-    return math.sin(x)
+    return x * np.sin(x * 2 * np.pi) if x < 0 else -x * np.sin(x * np.pi) + np.exp(x / 2) - np.exp(0)
 
-class Net(nn.Module):
+#Создаем модель нейронной сети
+class NeuralNetwork(nn.Module):
+    """
+    One input layer
+    One hidden layer
+    One output layer
+    """
     def __init__(self):
-        super().__init__()
-        self.layers = nn.Sequential(
-            nn.Linear(1, 70),
-            nn.Tanh(),
-            nn.Linear(70, 70),
-            nn.Tanh(),
-            nn.Linear(70, 1)
-        )
+        super(NeuralNetwork, self).__init__()
+        self.lin1 = nn.Linear(1, 70)
+        self.lin2 = nn.Linear(70, 70)
+        self.lin3 = nn.Linear(70, 1)
 
     def forward(self, x):
-        #x = F.sigmoid(self.lin1(x))
-        #x = F.tanh(self.lin2(x))
-        #x = F.relu(self.lin3(x))
-        return self.layers(x)
-
+        x = torch.tanh(self.lin1(x))
+        x = torch.tanh(self.lin2(x))
+        x = self.lin3(x)
+        return x
 
 if __name__ == "__main__":
-    x = np.linspace(-3, 3, 10000).reshape(-1, 1)
+    x = np.linspace(-5, 3, 4000).reshape(-1, 1)
     f = np.vectorize(f)
     # вычисляем вектор значений функции
     y = f(x)
 
     lr = 0.01
-    n_epochs = 800
+    n_epochs = 10000
     plot_every = n_epochs / 5
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     x_train_tensor = torch.from_numpy(x).float().to(device)
     y_train_tensor = torch.from_numpy(y).float().to(device)
 
-    model = Net()
+    model = NeuralNetwork()
     loss_fn = nn.MSELoss(reduction='mean')
     optimizer = optim.SGD(model.parameters(), momentum=0.9, lr=lr, nesterov=True)
 
